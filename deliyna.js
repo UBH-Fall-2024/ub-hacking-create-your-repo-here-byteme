5e88edba71dcf1f1fd586fbed26776e95f1da39b
deliynah <deliynahoang@gmail.com>
2024-11-10 09:30:55 -0500

    a

--- deliyna.js
+++ deliyna.js
@@ -1,22 +1,6 @@
-
-function comparePrices(req) {
-    const price = req.body.price;
+function madLib(){
+    var noun=document.getElementById("noun").value;
+    var p=document.getElementById("text");
     
-    function priceCalc(base, tax) {
-        return base + tax;
-    }
-
-    const base = 100; 
-    const tax = 0.1;  
-
-    const price_1 = price * priceCalc(base, tax);
-    const price_2 = price * priceCalc(base, tax);
-
-    if (price_1 < price_2) {
-        return price_1;
-    } else {
-        return price_2;
-    }
-}
-
-            
+    p.innerHTML= "Shhh Alice got a story to tell you. Once upon a time, a girl named Alice fell into a" + noun + ". A place known as the Great Wonderland " + noun;
+}      
